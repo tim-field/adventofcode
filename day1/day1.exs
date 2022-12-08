54ed2b7cc0d5ede315fe10512c65463ef92eba85
Tim Field <field.tim@gmail.com>
2022-12-08 17:04:01 +1300

    comments

--- day1/day1.exs
+++ day1/day1.exs
@@ -1,24 +1,27 @@
 defmodule Day1 do
   def run() do
     case File.read("input.txt") do
-      {:ok, body} -> read(String.split(body, "\n"))
+      {:ok, body} -> sort(String.split(body, "\n"))
       {:error, reason} -> reason
     end
   end
 
-  defp read(lines) do
-    # a better shape
-    # https://elixir-lang.org/getting-started/keywords-and-maps.html#nested-data-structures
+  defp sort(lines) do
     lines
-    |> Enum.reduce(%{1 => 0}, fn line, acc ->
-      idx = map_size(acc)
+    |> Enum.reduce([0], fn line, list ->
+      idx = length(list) - 1
 
       if line == "" do
-        Map.put(acc, idx + 1, 0)
+        # start a new list entry
+        list ++ [0]
       else
-        Map.put(acc, idx, Map.get(acc, idx) + String.to_integer(line))
+        # update the count for the entry at this idx
+        List.replace_at(list, idx, Enum.at(list, idx) + String.to_integer(line))
       end
     end)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
   end
 end
 
@@ -26,9 +29,4 @@ result = Day1.run()
 
 IO.puts(inspect(result))
 
-IO.puts(result[1])
-
-numbers = [5794, 5879, 4899, 6777, 5845, 1303, 6761, 1814, 6605, 4715, 2264, 2789]
-
-# Use the sum function to sum the numbers
-IO.puts(Enum.sum(numbers))
+IO.puts(Enum.sum(result))
