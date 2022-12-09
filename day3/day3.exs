9a5a699327a1a4cf4f0d48f18ee85508faebc85f
Tim Field <field.tim@gmail.com>
2022-12-09 22:14:27 +1300

    day 3 part 1

---
+++ day3/day3.exs
@@ -0,0 +1,36 @@
+defmodule Day3 do
+  def run(fileName) do
+    case File.read(fileName) do
+      {:ok, body} -> calc(String.split(body, "\n", trim: true))
+      {:error, reason} -> reason
+    end
+  end
+
+  defp priority(letter) do
+    cond do
+      # a-z is charcode [97-122] so sub 97 to get 1 for "a" [1-26]
+      String.match?(letter, ~r/[a-z]/) -> (String.to_charlist(letter) |> hd) - 96
+      # A-Z is charcode [65-90] so sub 64 to get 1 for 'A" ( then add 26 to offset the above ) [27-52]
+      String.match?(letter, ~r/[A-Z]/) -> (String.to_charlist(letter) |> hd) - 64 + 26
+    end
+  end
+
+  defp calc(lines) do
+    lines
+    |> Enum.map(fn line ->
+      splitAt = round(String.length(line) / 2)
+
+      # convert to a list then split in the middle
+      {c1, c2} =
+        String.codepoints(line)
+        |> Enum.split(splitAt)
+
+      c1
+      |> Enum.find(nil, fn l -> c2 |> Enum.member?(l) end)
+      |> priority()
+    end)
+    |> Enum.sum()
+  end
+end
+
+IO.puts(Day3.run("input.txt"))
