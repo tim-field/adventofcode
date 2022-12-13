0b1774d76ec2c03fa0d781ccc1c6a6359ff7d7c2
Tim Field <field.tim@gmail.com>
2022-12-13 21:07:10 +1300

    day 5 part 1

--- day5/day5.exs
+++ day5/day5.exs
@@ -1,33 +1,60 @@
 defmodule Day5 do
   def part1(fileName) do
-    fileName
-    |> readFile()
-    |> parseTable()
+    table =
+      readFile(fileName)
+      |> parseTable()
+
+    readFile(fileName)
+    |> parseInstructions()
+    |> Enum.reduce(table, fn instruction, table ->
+      performInstruction(instruction, table)
+    end)
+    |> Enum.map(&hd/1)
+    |> Enum.join("")
   end
 
   def parseTable(fileStream) do
     fileStream
+    # first 8 lines
     |> Enum.take(8)
-    |> Enum.reverse()
     |> Enum.map(fn line ->
       line
+      # convert to a list
       |> String.codepoints()
+      # drop initial [
       |> Enum.drop(1)
+      # take every forth char
       |> Enum.take_every(4)
     end)
-    |> Enum.reduce([], fn line, all ->
-      line
-      |> Enum.with_index()
-      |> Enum.reduce(all, fn {value, index}, acc ->
-        # IO.puts("value" <> value <> " index:" <> to_string(index) <>
-        # " val" <> inspect(val) <>
-        # " acc" <> inspect(acc))
-        case Enum.at(acc, index) do
-          nil -> List.insert_at(acc, index, [value])
-          _ -> List.replace_at(acc, index, Enum.at(acc, index) ++ [value])
-        end
-      end)
+    # convert lines to stacks
+    |> Enum.zip()
+    # remove empty strings
+    |> Enum.map(fn stack ->
+      stack
+      |> Tuple.to_list()
+      |> Enum.filter(fn value -> value != " " end)
     end)
+  end
+
+  def parseInstructions(fileStream) do
+    fileStream
+    |> Enum.drop(10)
+    |> Enum.map(fn line ->
+      # parse digits from instructions, sub 1 for 0 based offset
+      Regex.scan(~r/\d+/, line)
+      |> List.flatten()
+      |> Enum.map(fn value -> String.to_integer(value) - 1 end)
+    end)
+  end
+
+  def performInstruction(instruction, table) do
+    [move, from, to] = instruction
+
+    moveValues = Enum.at(table, from) |> Enum.slice(Range.new(0, move)) |> Enum.reverse()
+
+    table
+    |> List.replace_at(to, moveValues ++ Enum.at(table, to))
+    |> List.replace_at(from, Enum.at(table, from) |> Enum.drop(move + 1))
   end
 
   def readFile(fileName) do
